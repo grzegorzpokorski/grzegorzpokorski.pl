83efd523c34fe057c9c3eeb34b4d2affe0365ca5
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-17 15:47:09 +0100

    fix: add redirect previous /oferta/* pages to /oferta

--- next.config.js
+++ next.config.js
@@ -10,6 +10,25 @@ const nextConfig = {
     ],
     legacyBrowsers: false,
   },
+  async redirects() {
+    return [
+      {
+        source: "/oferta/tworzenie-stron-internetowych",
+        destination: "/oferta",
+        permanent: true,
+      },
+      {
+        source: "/oferta/kodowanie-projektow-graficznych",
+        destination: "/oferta",
+        permanent: true,
+      },
+      {
+        source: "/oferta/projektowanie-stron",
+        destination: "/oferta",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
