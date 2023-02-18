b0a6316acaee4a8b9737d6e37db094ccfb136753
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-18 12:03:41 +0100

    feat: add skip to the content link

--- src/components/templates/Layout/Layout.tsx
+++ src/components/templates/Layout/Layout.tsx
@@ -1,9 +1,11 @@
+import { SkipLink } from "@/components/atoms/SkipLink/SkipLink";
 import { Footer } from "@/components/organisms/Footer/Footer";
 import { MainHeader } from "@/components/organisms/MainHeader/MainHeader";
 import { ReactNode } from "react";
 
 export const Layout = ({ children }: { readonly children: ReactNode }) => (
   <>
+    <SkipLink />
     <MainHeader />
     {children}
     <Footer />
