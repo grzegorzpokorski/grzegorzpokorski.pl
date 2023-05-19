5c8b7c9aecc24976eb12afd2a373c1cc1788c404
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-05-19 12:17:03 +0200

    chore: upgrade nextjs to v13.4.3

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    legacyBrowsers: false,
-  },
   async redirects() {
     return [
       {
--- src/app/blog/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import { NotFoundPage } from "@/components/templates/NotFoundPage/NotFoundPage";
-
-// @to-do add meta data when nextjs team fix them and add ability to do that
-
-export default function NotFound() {
-  return <NotFoundPage />;
-}
